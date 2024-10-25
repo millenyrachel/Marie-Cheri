d702e5d685a130819681918009d2c9e71a424af5
millenyrachel <millenyrachelr@gmail.com>
2024-10-25 17:36:16 -0300

    Create pizzas.js

---
+++ pizzas.js
@@ -0,0 +1,71 @@
+pizzaJson = [
+  {
+    id: 1,
+    name: "",
+    img: "images/mussarela 1.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description: "Molho de tomate, camada dupla de mussarela e orégano",
+  },
+  {
+    id: 2,
+    name: "Gelato Caramelado",
+    img: "images/2.jpg",
+    price: [21.0, 24.0, 26.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, calabresa fatiada, cebola e orégano",
+  },
+  {
+    id: 3,
+    name: "",
+    img: "images/quatro queijo 3.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano",
+  },
+  {
+    id: 4,
+    name: "",
+    img: "images/brasileira.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano",
+  },
+  {
+    id: 5,
+    name: "",
+    img: "images/portuguesa.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano",
+  },
+  {
+    id: 6,
+    name: "",
+    img: "images/da casa.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description:
+      "Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano",
+  },
+  {
+    id: 7,
+    name: "",
+    img: "images/banana.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description: "Mussarela, banana, canela e açúcar",
+  },
+  {
+    id: 8,
+    name: "",
+    img: "images/morango.jpg",
+    price: [20.0, 23.0, 25.0],
+    sizes: ["6 fatias", "8 fatias", "12 fatias"],
+    description: "Creme de leite, lascas de chocolate e morangos",
+  },
+];

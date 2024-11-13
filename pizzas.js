8bd50fa6137379aae19db016956dbe42e6ea8472
millenyrachel <millenyrachelr@gmail.com>
2024-11-13 17:05:34 -0300

    Update pizzas.js

--- pizzas.js
+++ pizzas.js
@@ -33,5 +33,76 @@ pizzaJson = [
     sizes: ["300ml", "500ml", "700ml"],
     description:
       " frappuccino vibrante que captura a essência fresca dos morangos",
-  }
+  },
+  {
+    id: 5,
+    name: "Matcha Chill",
+    img: "images/matcha.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description:
+      "vibrante e suave mistura de chá matcha com uma base cremosa de leite (ou leite vegetal)",
+  },
+  {
+    id: 6,
+    name: "Matcha Fusion",
+    img: "images/mquente.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description: " combina a intensidade do café com a suavidade do matcha.",
+  },
+  {
+    id: 7,
+    name: "Comfort Cup",
+    img: "images/ccup.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description:
+      "Uma experiência de café reconfortante e quente, projetada para envolver você em um abraço acolhedor",
+  },
+  {
+    id: 8,
+    name: "Vanilla Dream",
+    img: "images/vani.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description:
+      " experiência de café luxuosa que captura a essência da baunilha doce",
+  },
+  {
+    id: 9,
+    name: "Cinnamon Delight",
+    img: "images/canela.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description:
+      "grãos de café torrados com o toque irresistível da canela fresca, criando uma experiência única a cada gole.",
+  },
+  {
+    id: 10,
+    name: "Brownie Fusion Box",
+    img: "images/browniegg.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300g", "450g", "700ml"],
+    description:
+      " Esta deliciosa caixa reúne pedaços de brownie macios e indulgentes",
+  },
+  {
+    id: 11,
+    name: "Choco Heaven Brownies",
+    img: "images/browniechoc.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description:
+      " combinação de chocolate amargo e ao leite, criando um equilíbrio perfeito entre o amargor suave e a doçura envolvente.",
+  },
+  {
+    id: 12,
+    name: "Double Chocolate Indulgence",
+    img: "images/browniewhite.jpg",
+    price: [13.0, 15.0, 18.0],
+    sizes: ["300ml", "500ml", "700ml"],
+    description:
+      "versão ainda mais deliciosa dessa sobremesa irresistível, agora com a combinação perfeita de chocolate branco e chocolate ao leite!",
+  },
 ];
